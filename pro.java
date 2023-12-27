d2593b26cc364e918afcde196ae07c7792956364
Venkatesh <venk01atesh@gmail.com>
2023-12-27 17:47:13 +0530

    k

---
+++ pro.java
@@ -0,0 +1,16 @@
+/**
+ * pro
+ */
+public class pro {
+
+    public static void main(String[] args) {
+    int x=1;
+    if(x!=1)
+    System.out.println(x++);
+    
+    System.out.println(x--);
+    System.out.println(x);
+
+
+            }
+        }
