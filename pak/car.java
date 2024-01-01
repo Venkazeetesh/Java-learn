976e861198be3dbb8caa3fd30cea44b0274a5a45
Venkatesh <venk01atesh@gmail.com>
2024-01-01 17:56:40 +0530

    g

---
+++ pak/car.java
@@ -0,0 +1,13 @@
+package pak;
+  public class car {
+    private String carname;
+    private int doors;
+    private int carprice;
+    private int carno;
+  public int setCarno(int carno){
+    return this.carno=carno;
+  }
+  public int getCarno(){
+    return this.carno;
+  }
+}
---
+++ pak/pro.java
@@ -0,0 +1,11 @@
+package pak;
+public class pro {
+
+  public static void main(String[] args) {
+    car car1=new car();
+    car1.setCarno(23555);
+   System.out.println(car1.getCarno());
+   System.out.println(car1.getCarno());
+  }
+}
+
